4f01c52cb28afa530ecc692b9278a5695e91abb7
htrcomputereducation <htrcomputereducation@gmail.com>
2025-07-22 21:33:43 +0530

    Create dashboard.php

---
+++ dashboard.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="hi">
+<head>
+  <meta charset="UTF-8">
+  <title>Dashboard - RaviPrint</title>
+</head>
+<body>
+  <h1>आपका स्वागत है डैशबोर्ड पर!</h1>
+  <p>यह लॉगिन के बाद का पेज है।</p>
+  <p><a href="login.php">लॉगआउट करें</a></p>
+</body>
+</html>
